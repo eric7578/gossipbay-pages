8f267a008d8272dc0daf776d72fa66dc4f83b635
ericyan <eric7578@gmail.com>
2020-09-16 00:23:29 +0800

    Add fetch-artifacts to download artifacts

---
+++ scripts/fetch-artifacts.js
@@ -0,0 +1,41 @@
+const { Octokit } = require('@octokit/rest');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const JSZip = require('jszip');
+
+const asyncWriteFile = promisify(fs.writeFile);
+
+const params = {
+  owner: process.env.GH_OWNER,
+  repo: process.env.GH_REPO,
+};
+
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
+
+const downloadArtifactAsJSON = async artifact => {
+  const { data } = await octokit.actions.downloadArtifact({
+    ...params,
+    artifact_id: artifact.id,
+    archive_format: 'zip',
+  });
+  const zip = new JSZip();
+  await zip.loadAsync(data);
+  const json = await zip.file(artifact.name).async('string');
+  await asyncWriteFile(
+    path.join(__dirname, '../.gh-artifacts', artifact.name),
+    json
+  );
+};
+
+(async () => {
+  const {
+    data: { artifacts },
+  } = await octokit.actions.listArtifactsForRepo(params);
+
+  await Promise.all(
+    artifacts.map(artifact => downloadArtifactAsJSON(artifact))
+  );
+})();

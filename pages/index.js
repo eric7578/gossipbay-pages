5168da152fd824e78edd251fc87a5344fcc725d1
ericyan <eric7578@gmail.com>
2020-09-19 15:10:20 +0800

    Init next project structure

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+  color: #333;
+`;
+
+const Index = props => {
+  return <Headline>Hello Gossipbay</Headline>;
+};
+
+export default Index;
